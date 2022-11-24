0993d59577ea93599bd2eff923ef62827459172b
jakegodsall <jake.edward.godsall@gmail.com>
2022-11-24 20:08:12 +0000

    added basic webpack config with sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,18 @@
 const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/'),
+    },
+    mode: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+};
